7d6aacd33f530b57f16bf8ff929447cd65e8c713
hongchan2 <ipeter@naver.com>
2020-03-28 20:30:00 +0900

    Create Project

---
+++ src/main/java/com/hongchan/snsspringboot/SnsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hongchan.snsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsSpringBootApplication.class, args);
+    }
+
+}
